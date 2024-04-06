0b5b0d2b17ad392bc208e18077d45ec9b4896763
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-04-06 21:19:03 +0300

    working and ugly login

--- routes.py
+++ routes.py
@@ -1,6 +1,45 @@
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
+import users
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        logged = users.login(username, password)
+        if logged[0]:
+            return redirect("/")
+        else:
+            return render_template("error.html", message=logged[1])
+
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Passwords are not the same")
+        success = users.register(username, password1)
+        if success[0]:
+            return redirect("/")
+        else:
+            return render_template("error.html", message=success[1])
