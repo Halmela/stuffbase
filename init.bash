ddeda8012787ab74e20d1a4455e651ff1d1dc470
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-04-12 18:37:54 +0300

    tweak initialization

--- init.bash
+++ init.bash
@@ -20,24 +20,33 @@ echo Install dependencies
 pip install -r ./requirements.txt
 
 echo Initialize database with name "stuffbase"
-psql -d stuffbase < schema.sql
+sql/init.bash
 
 echo lets create a base for .env file
 echo Is psql installed with local-pg script? y/n
 read -r
 if [[ $REPLY = "y" ]]; then
-  url="postgresql+psycopg2://stuffbase"
+  DATABASE_URL="postgresql+psycopg2://stuffbase"
 else
-  url="postgresql:///stuffbase"
+  DATABASE_URL="postgresql:///stuffbase"
 fi
 
-echo "database URL: $url"
+echo "database url: $DATABASE_URL"
 
 echo generate secret...
-secret=$(python3 -c 'import secrets
+SECRET_KEY=$(python3 -c 'import secrets
+print(secrets.token_hex(16))')  
+
+echo generate admin credientials
+echo the account will be created first time you go to /admin without logging in
+ADMIN_USERNAME=$(python3 -c 'import secrets
+print(secrets.token_hex(16))')  
+ADMIN_PASSWORD=$(python3 -c 'import secrets
 print(secrets.token_hex(16))')  
 
 cat << EOF > .env
-DATABASE_URL="$url"
-SECRET_KEY="$secret"
+DATABASE_URL="$DATABASE_URL"
+SECRET_KEY="$SECRET_KEY"
+ADMIN_USERNAME="$ADMIN_USERNAME"
+ADMIN_PASSWORD="$ADMIN_PASSWORD"
 EOF
